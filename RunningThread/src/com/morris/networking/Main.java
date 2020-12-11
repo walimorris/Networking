96fcca3408aefa9a76f0ffd3d55268d85932f604
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-12-10 23:44:54 -0800

    Update Main.java

--- RunningThread/src/com/morris/networking/Main.java
+++ RunningThread/src/com/morris/networking/Main.java
@@ -3,7 +3,6 @@ package com.morris.networking;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 1;
         Runnable run = () -> System.out.println("Running!");
         MyRunningThread thread = new MyRunningThread(run, 10);
         thread.run();
